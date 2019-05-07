559e8c917416bb52814f0e1e20f4135be4d17684
kevin JC Li <kevinjcli1994@gmail.com>
2019-05-07 18:55:10 -0400

    outsourced dropdown values to a different file

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -9,6 +9,9 @@ import { signupMutation } from '../graphql-queries/mutations'
 
 import { TextField, Button, FormHelperText, MenuItem } from '@material-ui/core'
 
+import { programs, campus, studyCohort, studyYear } from '../form-dropdown-values'
+
+
 const initialFormValues = {
 	userEmail: '',
 	userFullname: '',
@@ -20,98 +23,6 @@ const initialFormValues = {
 	whichStudyYear: '',
 	whichStudyCohort: '',
 }
-
-const programs = [
-	{
-		value: 'APP',
-		label: 'App Dev',
-	},
-	{
-		value: 'DM',
-		label: 'Digital Marketing',
-	},
-	{
-		value: 'UX',
-		label: 'UX Designer',
-	},
-	{
-		value: 'UI',
-		label: 'UI Designer',
-	},
-	{
-		value: 'WEB',
-		label: 'Web Dev',
-	},
-]
-
-const campus = [
-	{
-		value: 'LON',
-		label: 'London',
-	},
-	{
-		value: 'TOR',
-		label: 'Toronto',
-	},
-	{
-		value: 'VAN',
-		label: 'Vancouver',
-	},
-]
-
-const studyCohort = [
-	{
-		value: 'Q1',
-		label: 'Q1',
-	},
-	{
-		value: 'Q2',
-		label: 'Q2',
-	},
-	{
-		value: 'Q3',
-		label: 'Q3',
-	},
-	{
-		value: 'Q4',
-		label: 'Q4',
-	},
-]
-
-const studyYear = [
-	{
-		value: '2012',
-		label: '2012',
-	},
-	{
-		value: '2013',
-		label: '2013',
-	},
-	{
-		value: '2014',
-		label: '2014',
-	},
-	{
-		value: '2015',
-		label: '2015',
-	},
-	{
-		value: '2016',
-		label: '2016',
-	},
-	{
-		value: '2017',
-		label: '2017',
-	},
-	{
-		value: '2018',
-		label: '2018',
-	},
-	{
-		value: '2019',
-		label: '2019',
-	},
-]
 
 const Signup = () => {
 	const [redirecting, setRedirecting] = useState(false)
